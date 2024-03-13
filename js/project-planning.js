1f5857960656743fe69e00ec70bc5062096a470f
Daniel Martínez Duque <danielopq@gmail.com>
2024-03-13 00:56:00 +0000

    Web finished

--- js/project-planning.js
+++ js/project-planning.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+window.onload = start;
+
+function start(){
+	document.getElementById("menu-hamburger").addEventListener('click',showMbMenu,false);
+}
+
+function showMbMenu(){
+	if(document.getElementById("menu-hamburger").getAttribute("class") == "hamburger-icon"){
+		document.getElementById("menu-hamburger").setAttribute("class","close-icon");
+		document.getElementById("menu-mobile").style.display = "flex";
+	}else{
+		document.getElementById("menu-hamburger").setAttribute("class","hamburger-icon");
+		document.getElementById("menu-mobile").style.display = "none";
+	}
+}
